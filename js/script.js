a718275e89555a37fbd11f4f4c3d0cfd4e1cac01
AlexandrMidved-2023 <0megakillerman0@gmail.com>
2023-06-30 21:47:46 +0300

    less29

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let academ = 'Beetroot';
+console.log(academ);
+let courses = 'FrontEnd';
+console.log(courses);
+let country = 'Ukrain';
+console.log(country);
+let fullName = 'Aleksandr Midved';
+console.log(fullName);
+let $ = '1000000$';
+console.log($);
+
+let all = academ + "\n" + courses + "\n" + country + "\n" + fullName + "\n" + $;
+console.log(all);
+
+console.log(academ + "\n" + courses + "\n" + country + "\n" + fullName + "\n" + $);
